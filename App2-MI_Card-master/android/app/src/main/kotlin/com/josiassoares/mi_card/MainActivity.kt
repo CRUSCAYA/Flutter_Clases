d55a5bc0a398dd78375e8a984a60762c67362300
cruscaya <ronaldo12roni@gmail.com>
2023-09-15 11:04:56 -0500

    carrusel de imagenes

--- App2-MI_Card-master/android/app/src/main/kotlin/com/josiassoares/mi_card/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.josiassoares.mi_card
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- app_state/lib/main.dart
+++ app_state/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:app_state/home_page.dart';
+import 'package:app_state/test1_page.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       // Configura la pantalla principal de la aplicación
-      home: HomePage(),
+      //home: HomePage(),
+      home:Test1Page(),
     ),
   );
 }
